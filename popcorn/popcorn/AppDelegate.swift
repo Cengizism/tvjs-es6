83160397e5e83dc827f16aaab53caa0f57d56f0d
Cengiz Ulusoy <ulusoy.cengiz@gmail.com>
2015-11-20 17:06:53 +0100

    Make first working version ES6

--- popcorn/popcorn/AppDelegate.swift
+++ popcorn/popcorn/AppDelegate.swift
@@ -1,64 +1,81 @@
-//
-//  AppDelegate.swift
-//  tvos
-//
-//  Created by Cengiz Ulusoy on 15/11/15.
-//  Copyright © 2015 Cengiz Ulusoy. All rights reserved.
-//
+/*
+Copyright (C) 2015 Apple Inc. All Rights Reserved.
+See LICENSE.txt for this sample’s licensing information
+
+*/
 
 import UIKit
 import TVMLKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+    // MARK: Properties
     
     var window: UIWindow?
+    
     var appController: TVApplicationController?
     
+    static let TVBaseURL = "http://localhost:9001/"
+    
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)dist/application.js"
+    
+    // MARK: UIApplication Overrides
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
+        /*
+        Create the TVApplicationControllerContext for this application
+        and set the properties that will be passed to the `App.onLaunch` function
+        in JavaScript.
+        */
         let appControllerContext = TVApplicationControllerContext()
         
-        let javascriptURL = NSURL(string: "http://localhost:9001/dist/application.js")
+        /*
+        The JavaScript URL is used to create the JavaScript context for your
+        TVMLKit application. Although it is possible to separate your JavaScript
+        into separate files, to help reduce the launch time of your application
+        we recommend creating minified and compressed version of this resource.
+        This will allow for the resource to be retrieved and UI presented to
+        the user quickly.
+        */
+        if let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) {
+            appControllerContext.javaScriptApplicationURL = javaScriptURL
+        }
         
-        appControllerContext.javaScriptApplicationURL = javascriptURL!
-        if let options = launchOptions {
-            for (kind, value) in options {
-                if let kindStr = kind as? String {
-                    appControllerContext.launchOptions[kindStr] = value
-                }
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        if let launchOptions = launchOptions as? [String: AnyObject] {
+            for (kind, value) in launchOptions {
+                appControllerContext.launchOptions[kind] = value
             }
         }
         
-        self.appController = TVApplicationController(context: appControllerContext, window: self.window, delegate: self)
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
         
         return true
     }
     
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // MARK: TVApplicationControllerDelegate
+    
+    func appController(appController: TVApplicationController, didFinishLaunchingWithOptions options: [String: AnyObject]?) {
+        print("\(__FUNCTION__) invoked with options: \(options)")
     }
     
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func appController(appController: TVApplicationController, didFailWithError error: NSError) {
+        print("\(__FUNCTION__) invoked with error: \(error)")
+        
+        let title = "Error Launching Application"
+        let message = error.localizedDescription
+        let alertController = UIAlertController(title: title, message: message, preferredStyle:.Alert )
+        
+        self.appController?.navigationController.presentViewController(alertController, animated: true, completion: { () -> Void in
+            // ...
+        })
     }
     
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    func appController(appController: TVApplicationController, didStopWithOptions options: [String: AnyObject]?) {
+        print("\(__FUNCTION__) invoked with options: \(options)")
     }
-    
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    
 }
-
